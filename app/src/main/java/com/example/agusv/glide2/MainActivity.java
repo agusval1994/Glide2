7e041883a58436359491ee4268ddc3e8dd2f2baf
agusval1994 <agusval1994@hotmail.com>
2018-04-19 17:06:02 -0300

    --

---
+++ app/src/main/java/com/example/agusv/glide2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.agusv.glide2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView)findViewById(R.id.imageView);
+
+        Glide.with(this)
+                .load(R.drawable.asteroide)
+                .into(imageView);
+    }
+}
